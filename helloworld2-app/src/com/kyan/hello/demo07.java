bfd96e7d7e533fde8f87876fd3b074bada79a525
kyan1823 <yan050520@gmail.com>
2024-11-18 14:33:49 +0800

    Changes

--- helloworld2-app/src/com/kyan/hello/demo07.java
+++ helloworld2-app/src/com/kyan/hello/demo07.java
@@ -4,44 +4,45 @@ import java.util.Scanner;
 
 public class demo07 {
     public static void main(String[] args) {
-        score(8);
+        System.out.println(score(6,100,0));
     }
 
-    public static int[] score(int n) {
+    public static int score(int n, int max, int min) {
         int[] arr = new int[n];
         Scanner sc = new Scanner(System.in);
-        System.out.println("0-100,int");
+        System.out.println("between " + min + " and " + max);
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            if (a > 100 || a < 0) {
-                System.out.println("0-100,int");
-                i--;
+            if (a > max || a < min) {
+                System.out.println("between " + min + " and " + max);
+                return 0;
             }
             arr[i] = a;
         }
-        return arr;
+        int sum = sum(arr);
+        int m = maxplusmin(arr,max,min);
+        return (sum - m)/(n - 2);
     }
 
-    public static int score() {
-        int[] arr = score(8);
-        int max = 0;
-        int min = 101;
+    public static int sum(int[] arr) {
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+            sum += arr[i];
         }
+        return sum;
+    }
+
+    public static int maxplusmin(int[] arr, int max, int min) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
+            if (arr[i] >=  min) {
                 min = arr[i];
             }
         }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= max) {
+                max = arr[i];
+            }
+        }
         return max + min;
-    };
-
-    public static int zzz() {
-        int i = score();
-        score(n);
-
     }
 }
