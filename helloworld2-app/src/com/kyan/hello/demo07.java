724bd0b10f374ba9a32e439fd8e345c62f5600dd
kyan1823 <yan050520@gmail.com>
2024-11-19 09:29:10 +0800

    添加注释

--- helloworld2-app/src/com/kyan/hello/demo07.java
+++ helloworld2-app/src/com/kyan/hello/demo07.java
@@ -37,8 +37,6 @@ public class demo07 {
             if (arr[i] >=  min) {
                 min = arr[i];
             }
-        }
-        for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= max) {
                 max = arr[i];
             }
